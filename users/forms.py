bee644e92978ba089b19b886fe2a351c9f6f0c44
lunatic280 <https://github.com/lunatic280>
2024-08-20 18:36:45 +0900

    이미지 적용

--- users/forms.py
+++ users/forms.py
@@ -10,19 +10,12 @@ class LoginForm(forms.Form):
 class SignupForm(forms.Form):
     username = forms.CharField()
     password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
 
     def clean_username(self):
         username = self.cleaned_data["username"]
         if User.objects.filter(username=username).exists():
             raise ValidationError(f"입력한 사용자명({username})은 이미 사용중입니다")
         return username
-
-    def clean(self):
-        password1 = self.cleaned_data["password1"]
-        password2 = self.cleaned_data["password2"]
-        if password1 != password2:
-            self.add_error("password2", "비밀번호와 비밀번호 확인란의 값이 다릅니다")
 
     def save(self):
         username = self.cleaned_data["username"]
